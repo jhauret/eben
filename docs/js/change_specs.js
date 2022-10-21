24c8a2ff2c554ccf1a4c6d04d9c371befd35a4fb
Eric Bavu <eric.bavu@gmail.com>
2022-10-21 14:04:05 +0200

    Modifications Eric

---
+++ docs/js/change_specs.js
@@ -0,0 +1,34 @@
+function toggle_ref() {
+   document.getElementById('spec').src = 'img/spec_ref.png' ;
+   document.getElementById("spec_zoom").style.backgroundImage = "url('img/only_ref.png')";
+}
+
+function toggle_inear() {
+   document.getElementById('spec').src = 'img/spec_inear.png' ;
+   document.getElementById("spec_zoom").style.backgroundImage = "url('img/only_inear.png')";
+}
+
+function toggle_hifigan() {
+   document.getElementById('spec').src = 'img/spec_hifigan.png' ;
+   document.getElementById("spec_zoom").style.backgroundImage = "url('img/only_hifigan.png')";
+}
+
+function toggle_kuleshov() {
+   document.getElementById('spec').src = 'img/spec_kuleshov.png' ;
+   document.getElementById("spec_zoom").style.backgroundImage = "url('img/only_kuleshov.png')";
+}
+
+function toggle_seanet() {
+   document.getElementById('spec').src = 'img/spec_seanet.png' ;
+   document.getElementById("spec_zoom").style.backgroundImage = "url('img/only_seanet.png')";
+}
+
+function toggle_eben() {
+   document.getElementById('spec').src = 'img/spec_eben.png' ;
+   document.getElementById("spec_zoom").style.backgroundImage = "url('img/only_eben.png')";
+}
+
+function toggle_streaming() {
+   document.getElementById('spec').src = 'img/spec_streaming.png' ;
+   document.getElementById("spec_zoom").style.backgroundImage = "url('img/only_streaming.png')";
+}
