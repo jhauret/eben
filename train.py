2c16f648e47c35d3236c27c787d94682214ca579
Julien Hauret <53187038+jhauret@users.noreply.github.com>
2022-11-02 11:35:52 +0100

    Update train.py

--- train.py
+++ train.py
@@ -22,10 +22,10 @@ def train():
     discriminator: torch.nn.Module = DiscriminatorEBENMultiScales()
     eben: LightningModule = EBEN(generator=generator, discriminator=discriminator,
                                  lr=0.0003, betas=(0.5, 0.9))
-    trainer: Trainer = Trainer(gpus=1)
+    trainer: Trainer = Trainer(gpus=1, max_epochs=13, enable_checkpointing=False)
 
     # Fit
-    trainer.fit(model=eben, datamodule=datamodule, max_epochs=13, enable_checkpointing=False)
+    trainer.fit(model=eben, datamodule=datamodule)
 
 
 if __name__ == '__main__':
